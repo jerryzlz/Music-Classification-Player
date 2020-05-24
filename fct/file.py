dfbb8304f8e1949e4b62189868f2ae1a956e2dbb
Lizhong <jerryzlz4@hotmail.com>
2020-05-24 15:23:39 +0800

    Add files via upload

---
+++ fct/file.py
@@ -0,0 +1,79 @@
+import os, shutil
+
+
+def dir_name(dir_path):
+    """
+    转换零时文件夹、输入文件夹路径
+    :param dir_path: 主目录路径
+    :return:[tmp_dir, input_dir]
+    """
+    tmp_dir = dir_path + "tmp\\"
+    input_dir = dir_path + "input\\"
+    res_dir = dir_path + "result\\"
+    return tmp_dir, input_dir, res_dir
+
+
+def del_dir(dir_path):
+    """
+    删除文件夹
+    :param dir_path: 目录路径
+    :return:
+    """
+    os.removedirs(dir_path)
+
+
+def del_files(dir_path):
+    """
+    删除目录下所有文件
+    :param dir_path: 目录路径
+    :return:
+    """
+    filename = os.listdir(dir_path)
+    for f in filename:
+        os.remove(dir_path + f)
+
+
+def create_dir(dir_path):
+    """
+    创建目录
+    :param dir_path: 目录路径
+    :return:
+    """
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+
+
+def create_genres_dir(dir_path, list):
+    """
+    创建分类文件夹
+    :param dir_path: 目录路径
+    :param list: 音乐类型列表
+    :return:
+    """
+    for l in list:
+        create_dir(dir_path + l)
+
+
+def move_file(source, destination, file_name, label):
+    """
+    移动已经分类完成的音乐
+    :param source: 输入文件路径
+    :param destination: 分类文件夹路径
+    :param file_name: 输入文件名列表
+    :param label: 分类结果列表
+    :return:
+    """
+    for f in range(len(file_name)):
+        shutil.move(source + file_name[f], destination + label[f])
+
+
+def first_run(status):
+    """
+    获取settings中的first_run返回布尔值
+    :param status: 输入参数
+    :return: True/False
+    """
+    if status == "1":
+        return True
+    else:
+        return False
---
+++ fct/welcome.py
@@ -0,0 +1,63 @@
+import sys
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+
+def window():
+    """
+    初次打开软件的欢迎界面
+    :return:
+    """
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+
+    btn_min = QtWidgets.QPushButton(w)
+    btn_min.setStyleSheet('background-color:#00ff00;border-radius:5px;')
+    btn_min.setGeometry(0, 0, 23, 23)
+    btn_min.setText("")
+    btn_min.move(1160, 60)
+
+    btn_close = QtWidgets.QPushButton(w)
+    btn_close.setStyleSheet('background-color:#ff0000;border-radius:5px;')
+    btn_close.setGeometry(0, 0, 23, 23)
+    btn_close.setText("")
+    btn_close.move(1200, 60)
+
+    logo = QtWidgets.QLabel(w)
+    logo.setPixmap(QtGui.QPixmap("images\Sangeetic(1).png"))
+    logo.move(0, 50)
+
+    wel = QtWidgets.QLabel(w)
+    wel.setText("欢   迎")
+    wel.setStyleSheet("color:#02e494;font-size:45px;font-family:微软雅黑;")
+    wel.move(200, 400)
+
+    before = QtWidgets.QLabel(w)
+    before.setText("使用须知")
+    before.setStyleSheet("color:#02e494;font-size:33px;font-family:微软雅黑;")
+    before.move(500, 200)
+
+    noti = QtWidgets.QLabel(w)
+    noti.setText("本软件基于深度学习进行分类，由于音乐种类众多并且通\n"
+                 "常一首歌中包含多种音乐风格无法涵盖所有的情况。分类\n"
+                 "结果仅供参考，感谢您的使用。")
+    noti.setStyleSheet("color:#fff;font-size:28px;font-family:微软雅黑;")
+    noti.move(500, 300)
+
+    next = QtWidgets.QPushButton(w)
+    next.setText("进入软件")
+    next.setGeometry(1050, 600, 150, 50)
+    next.setStyleSheet("color:#fff;background-color:rgb(28, 37, 41);font-size:28px;font-family:微软雅黑;")
+    next.clicked.connect(w.close)
+    btn_min.clicked.connect(w.showMinimized)
+    btn_close.clicked.connect(w.close)
+
+    w.setStyleSheet("background-color:rgb(28, 37, 41);")
+    w.setWindowTitle("欢迎")
+    w.setGeometry(0, 20, 1280, 720)
+    w.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+    w.show()
+    app.exec()
+
+
+if __name__ == '__main__':
+    window()
